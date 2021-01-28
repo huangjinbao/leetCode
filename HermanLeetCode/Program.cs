7469db453dbc08c85c245908d23303a72027ec7c
herman <hjb_works@163.com>
2021-01-28 18:48:30 +0800

    提交

--- HermanLeetCode/Program.cs
+++ HermanLeetCode/Program.cs
@@ -7,7 +7,7 @@ namespace HermanLeetCode
         private static void Main(string[] args)
         {
             var s = new string[] { "5", "2", "C", "D", "+" };
-            var calPoints = Arithmetic_682_BaseBallGame.CalPoints(s);
+            var calPoints = Arithmetic_0682_BaseBallGame.CalPoints(s);
         }
     }
 }
